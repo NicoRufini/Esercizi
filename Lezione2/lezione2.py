7df474df6103b8f82071a9d8013d17a62cb93dcb
nicorufini <nikorufini@gmail.com>
2024-04-18 13:26:58 +0200

    Esercizi svolti: 1°, 2°, 3°,  4°, 5°

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -39,3 +39,11 @@ print(f"{famous_person} once said: \"{message}\"")
 filename: str = "python_notes.txt"
 
 print(filename.removesuffix(".txt"))
+
+#3-1. Names: Store the names of a few of your friends in a list called names. 
+#Print each person’s name by accessing each element in the list, one at a time.
+
+friends: list = ["Giampiero", "Giancarlo", "Lucrezio"]
+
+for i in friends:
+    print(i)
