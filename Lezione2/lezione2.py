16dfdb29b3ec221e86677ca3cabfcfdcc394dfd6
nicorufini <nikorufini@gmail.com>
2024-04-18 13:21:45 +0200

    Esercizi svolti: 1°,  2°,  3°,  4°

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -31,3 +31,11 @@ famous_person: str= "Albert Einstein"
 message: str= "A person who never made a mistake never tried anything new."
 
 print(f"{famous_person} once said: \"{message}\"")
+
+#2-8. File Extensions: Python has a removesuffix() method that works exactly like removeprefix(). 
+#Assign the value 'python_notes.txt' to a variable called filename. Then use the removesuffix() method to display the filename without the file extension, 
+#like some file browsers do.
+
+filename: str = "python_notes.txt"
+
+print(filename.removesuffix(".txt"))
