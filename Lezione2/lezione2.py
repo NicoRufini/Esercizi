07fc4fd2cb8004fe82e55290ae12f7d27ec5c57e
nicorufini <nikorufini@gmail.com>
2024-04-18 13:34:08 +0200

    Esercizi svolti: 1°, 2°, 3°, 4°, 5°, 6°

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -47,3 +47,10 @@ friends: list = ["Giampiero", "Giancarlo", "Lucrezio"]
 
 for i in friends:
     print(i)
+
+#3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. 
+#The text of each message should be the same, but each message should be personalized with the person’s name.
+
+print(f"Hi {friends[0]}, how are you?")
+print(f"Hi {friends[1]}, how are you?")
+print(f"Hi {friends[2]}, how are you?")
