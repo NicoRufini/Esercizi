564bbb39ee6f8669a03a808026233409eff30dfa
nicorufini <nikorufini@gmail.com>
2024-04-18 13:09:37 +0200

    Esercizi svolti: 1°, 2°, 3°

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -22,4 +22,12 @@ print(name)
 #2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following,
 #including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 
-print("Albert Einstein once said, “A person who never made a mistake never tried anything new.”")
+print("Albert Einstein once said, \"A person who never made a mistake never tried anything new.\"")
+
+#2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. 
+#Then compose your message and represent it with a new variable called message. Print your message. 
+
+famous_person: str= "Albert Einstein"
+message: str= "A person who never made a mistake never tried anything new."
+
+print(f"{famous_person} once said: \"{message}\"")
