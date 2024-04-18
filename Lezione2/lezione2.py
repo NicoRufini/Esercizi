28d6367b5bede87aa5b7b6bbc243a53bb0db96b5
nicorufini <nikorufini@gmail.com>
2024-04-18 12:54:58 +0200

    Risolto 2° esercizio Risolto 3° esercizio

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -8,3 +8,18 @@
 name: str = "Bob"
 
 print(f"Hello {name}, would you like to learn some Python today?")
+
+#2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
+
+name: str = "Luca"
+
+print(name.lower())
+
+print(name.upper())
+
+print(name)
+
+#2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following,
+#including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+print("Albert Einstein once said, “A person who never made a mistake never tried anything new.”")
