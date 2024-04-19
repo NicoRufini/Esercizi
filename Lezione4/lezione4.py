ee1c5072bf37a7412889638ace4efc226a1a0685
nicorufini <nikorufini@gmail.com>
2024-04-19 12:55:46 +0200

    Esercizi svolti:  1°

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -8,3 +8,17 @@ def subtract(a: int, b: int) -> int:
     return a - b
 
 print(subtract(5, 2))
+
+
+#Write a function check_value(), which takes a number as an argument.
+#Using if / else, the function should print whether the number is bigger, smaller, or equal to 5.
+
+def check_value(n: int):
+    if n > 5:
+        print("The number is bigger than 5")
+    elif n < 5:
+        print("The number is smaller than 5")
+    else:
+        print("The number is equal to 5")    
+
+check_value(7)
