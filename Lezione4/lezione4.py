0e2b0438befce34e78156edb8086e9daa5f9068f
nicorufini <nikorufini@gmail.com>
2024-04-19 12:00:54 +0200

    L'esercizio sulle funzioni è stato svolto

---
+++ Lezione4/lezione4.py
@@ -0,0 +1,10 @@
+#Let’s try to define a function named subtract ourselves:
+#- It should take 2 parameters.
+#- Inside the function, it should subtract the two.
+#- Then, return the result.
+#After you defined it, call the function with some arguments!
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+print(subtract(5, 2))
