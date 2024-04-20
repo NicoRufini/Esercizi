976179114cd0578a287a42e564634a056f7ccca1
NicoRufini <nikorufini@gmail.com>
2024-04-20 15:23:41 +0200

    Esercizi svolti: 1°, 2°, 3°, 4°, 5°, 6°, 7°, 8°, 9°

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -83,7 +83,14 @@ for i in people_I_would_like_to_invite:
 
 #3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out 
 #a new set of invitations. You’ll have to think of someone else to invite.
-#• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+#• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest 
+#  who can’t make it.
 #• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
 #• Print a second set of invitation messages, one for each person who is still in your list.
 
+print(f"{people_I_would_like_to_invite[0]} won't be able to go to the dinner.")
+
+people_I_would_like_to_invite[0] = "Amy Winehouse"
+
+for i in people_I_would_like_to_invite:
+    print(f"Hi {i}, I'd like to invite you to my house for dinner on Thursday. Kind regards, Nicolò Rufini.")
