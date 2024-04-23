a53f3b86192db8a5ca745e0bcbd4b36177a46770
nicorufini <nikorufini@gmail.com>
2024-04-23 18:53:02 +0200

    Esercizi svolti: 1°, 2°, 3°, 4°, 5°, 6°, 7°, 8°, 9°, 10°, 11°, 12°, 13° , 14°, 15°, 16°, 17°, 18°, 19°, 20°, 21°, 22°, 23°, 24°

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -329,3 +329,10 @@ for i in cities:
 #6-12. Extensions: We’re now working with examples that are complex enough that they can be extended in any number of ways. 
 #Use one of the example programs from this chapter, and extend it by adding new keys and values, 
 #changing the context of the program, or improving the formatting of the output.
+
+fruits_and_colors: dict = {"Apple" : "Red", "Watermelon" : "Green", "Peach" : "Orange", "Coconut" : "Brown"}
+
+for i in fruits_and_colors:
+    fruits_and_colors.update({i : "Yellow"})
+
+print(fruits_and_colors)
