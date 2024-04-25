6b1d45f5494cbec77707f24bdbf1c8aa64fbfe92
NicoRufini <nikorufini@gmail.com>
2024-04-25 15:52:12 +0200

    Esercizi svolt: 1°, 2°, 3°, 4°, 5°, 6°, 7°, 8° , 9°, 10°, 11°, 12° non fatto

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -96,9 +96,35 @@ for i in cubes:
 #• Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
 
 x = slice(3)
-y = slice(4, )
-z = slice()
+y = slice(3, 7)
+z = slice(7, 10)
 
-print(" The first three items in the list are:", )
+print("The list is:", multiples_of_3)
+print("The first three items in the list are:", multiples_of_3[x])
+print("Three items from the middle of the list are:", multiples_of_3[y])
+print("The last three items in the list are:", multiples_of_3[z])
 
+
+#4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. Make a copy of the list of pizzas, and call it friend_pizzas. 
+#Then, do the following:
+#• Add a new pizza to the original list.
+#• Add a different pizza to the list friend_pizzas.
+#• Prove that you have two separate lists. Print the message My favorite pizzas are:
+#and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are: 
+#and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
+
+friend_pizzas: list = ["Four cheese pizza", "Sausage and fries pizza", "Margherita pizza"]
+
+pizzas.append("Neapolitan")
+friend_pizzas.append("New York-style")
+
+for i in pizzas:
+    print("My favorite pizzas are:", i)
+print("\n")
+for i in friend_pizzas:
+    print("My friend's favorite pizzas are:", i)
+
+
+#4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, to save space. 
+#Choose a version of foods.py, and write two for loops to print each list of foods.
 
