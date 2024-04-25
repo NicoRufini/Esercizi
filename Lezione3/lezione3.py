e0c11f25eb996553dd1f2acff95f867546c9b3a7
NicoRufini <nikorufini@gmail.com>
2024-04-25 13:22:45 +0200

    Esercizi svolt: 1°, 2°, 3°, 4°, 5°, 6°, 7°, 8°

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -52,4 +52,28 @@ print(f"First item: {one_million[0]}, and last item: {one_million[-1]}")
 print(f"min: {min(one_million)}. \nmax: {max(one_million)}. \nadd: {sum(one_million)}.")
 
 
-#
+#4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. 
+#Use a for loop to print each number.
+
+odd_numbers: list = [i for i in range(1, 21, 2)]
+
+for i in odd_numbers:
+    print(i)
+
+
+#4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
+
+multiples_of_3: list = [i for i in range(3, 31, 3)]
+
+for i in multiples_of_3:
+    print(i)
+
+
+#4-8. Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. 
+#Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out 
+#the value of each cube.
+
+cubes: list = [i**2 for i in range(1, 11)]
+
+for i in cubes:
+    print(i)
