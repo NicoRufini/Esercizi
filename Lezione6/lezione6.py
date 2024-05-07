c1c9b6eceb6dccd913bd3b7058b13d938dac657d
nicorufini <nikorufini@gmail.com>
2024-05-07 19:29:22 +0200

    frase..

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -28,3 +28,52 @@ class Student:
 me = Student("Nicolò", "Fullstack")
 on_the_left = Student("Alessandro", "Fullstack") #age 25
 on_the_right = Student("Gianmarco", "Fullstack") #age 27
+
+#da finire
+
+
+class Animal:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.legs = None
+    
+    def set_legs(self, legs: int):
+        self.legs = legs
+
+    def get_legs(self):
+        return f"there are {self.legs} legs."
+
+horse = Animal("Horse")
+horse.set_legs(4)
+seahorse = Animal("Seahorse")
+seahorse.set_legs(0)
+
+print(horse.name, horse.legs, seahorse.name, seahorse.legs)
+
+horse.legs = 3
+seahorse.legs = 2
+
+print(horse.legs, seahorse.legs)
+print(horse.get_legs() + "\n" + seahorse.get_legs())
+
+
+class Food:
+    def __init__(self, name: str, price: int, description) -> None:
+        self.name = name
+        self.price = price
+        self.description = description
+
+hampurger = Food("Hambuger", 12, "It has a good taste.")
+pasta = Food("Pizza", 2, "")
+pizza = Food("Pizza", 20, "")
+
+class Menu:
+    def __init__(self, foods: list) -> None:
+        self.foods = foods
+        foods = []
+
+    def add_food():
+        Menu.append()
+    
+    def remove_food():
+        Menu.remove()
