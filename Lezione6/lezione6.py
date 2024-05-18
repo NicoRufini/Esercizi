e003b8158b6c205ecc9fd54462deb1fa4e46ec21
NicoRufini <nikorufini@gmail.com>
2024-05-18 19:38:40 +0200

    Esercizi svolti: 1°, 2°, 3°, 4°, 5°, 6°, 7°, 8°

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -201,3 +201,134 @@ print("number_served with add_number_served:", restaurant.number_served)
 #Print login_attempts again to make sure it was reset to 0.
 
 print("(9-5)", "-" * 50)
+
+class User:
+    def __init__(self, first_name: str, last_name: str, email: str, phone_number: str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.login_attempts: int = 0
+
+    def describe_user(self) -> str:
+        print(f"name = {self.first_name}, surname = {self.last_name}, email = {self.email}, phone number = {self.phone_number}")
+    
+    def greet_user(self) -> str:
+        print(f"Hi {self.first_name} {self.last_name}!")
+
+    def increment_login_attempts(self) -> int:
+        self.login_attempts += 1
+
+    def reset_login_attempts(self) -> int:
+        self.login_attempts = 0
+
+#User from 9-3
+user_1: User = User("Giampiero", "Bonadonna", "fakeemail@gmail.com", "555-734 586 92 92")
+
+user_1.increment_login_attempts()
+user_1.increment_login_attempts()
+user_1.increment_login_attempts()
+print("login_attempts with increment_login_attempts:", user_1.login_attempts)
+
+user_1.reset_login_attempts()
+print("login_attempts with reset_login_attempts:", user_1.login_attempts)
+
+
+#9-6. Ice Cream Stand: An ice cream stand is a specific kind of restaurant. 
+#Write a class called IceCreamStand that inherits from the Restaurant class you wrote in Exercise 9-1 
+#or Exercise 9-4. Either version of the class will work; just pick the one you like better. 
+#Add an attribute called flavors that stores a list of ice cream flavors. Write a method that displays these flavors. 
+#Create an instance of IceCreamStand, and call this method. 
+
+print("(9-6)", "-" * 50)
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name: str, cuisine_type: str, flavors: list[str]) -> None:
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = flavors
+
+    def display_flavors(self) -> str:
+        string_flavors: str = "Flavors: "
+        for i in range(len(self.flavors)):
+            if self.flavors[i] != self.flavors[-1]:
+                string_flavors += f"{self.flavors[i].__str__()}, "
+
+        string_flavors += f"{self.flavors[-1].__str__()}"
+        
+        print(string_flavors)
+
+ice_cream_stand: IceCreamStand = IceCreamStand("Moriyama", "Japanese", ["Chocolate", "Coffee", "Strawberry"])
+ice_cream_stand.display_flavors()
+
+
+#9-7. Admin: An administrator is a special kind of user. 
+#Write a class called Admin that inherits from the User class you wrote in Exercise 9-3 or Exercise 9-5. 
+#Add an attribute, privileges, that stores a list of strings like "can add post", "can delete post", "can ban user", 
+#and so on. Write a method called show_privileges() that lists the administrator’s set of privileges. 
+#Create an instance of Admin, and call your method.
+
+print("(9-7)", "-" * 50)
+
+class Admin(User):
+    def __init__(self, first_name: str, last_name: str, email: str, phone_number: str) -> None:
+        super().__init__(first_name, last_name, email, phone_number)
+        self.privileges: list[str] = ["can add post", "can delete post", "can ban user"]
+
+    def show_privileges(self)  -> str:
+        string_privileges: str = "Your privileges: "
+        for i in range(len(self.privileges)):
+            if self.privileges[i] != self.privileges[-1]:
+                string_privileges += f"{self.privileges[i].__str__()}, "
+
+        string_privileges += f"{self.privileges[-1].__str__()}"
+        
+        print(string_privileges)
+
+admin_1: Admin = Admin("Giampiero", "Bonadonna", "fakeemail@gmail.com", "555-734 586 92 92")
+admin_1.show_privileges()
+
+
+#9-8. Privileges: Write a separate Privileges class. The class should have one attribute, privileges, 
+#that stores a list of strings as described in Exercise 9-7. Move the show_privileges() method to this class. 
+#Make a Privileges instance as an attribute in the Admin class. 
+#Create a new instance of Admin and use your method to show its privileges.
+
+print("(9-8)", "-" * 50)
+
+class Privileges:
+    def __init__(self, privileges: list[str]) -> None:
+        self.privileges = privileges
+        #self.privileges: list[str] = ["can add post", "can delete post", "can ban user"]
+    
+    def show_privileges(self)  -> str:
+        string_privileges: str = "Your privileges: "
+        for i in range(len(self.privileges)):
+            if self.privileges[i] != self.privileges[-1]:
+                string_privileges += f"{self.privileges[i].__str__()}, "
+
+        string_privileges += f"{self.privileges[-1].__str__()}"
+        
+        print(string_privileges)
+
+class Admin(User):
+    def __init__(self, first_name: str, last_name: str, email: str, phone_number: str, privileges: Privileges) -> None:
+        super().__init__(first_name, last_name, email, phone_number)
+        self.privileges = privileges
+        #self.privileges: list[str] = ["can add post", "can delete post", "can ban user"]
+
+    '''def show_privileges(self)  -> str:
+        string_privileges: str = "Your privileges: "
+        for i in range(len(self.privileges)):
+            if self.privileges[i] != self.privileges[-1]:
+                string_privileges += f"{self.privileges[i].__str__()}, "
+
+        string_privileges += f"{self.privileges[-1].__str__()}"
+        
+        print(string_privileges)'''
+    
+
+privileges_1: Privileges = Privileges(["can add post", "can delete post"])
+admin_2: Admin = Admin("Lucrezia", "Martino", "anotherfakeemail@gmail.com", "555-9834 7215 06", privileges_1)
+
+admin_2.privileges.show_privileges()
+
