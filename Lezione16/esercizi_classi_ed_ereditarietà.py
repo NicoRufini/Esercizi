1b33ef3229c22b3479986e833dfbdf52db349263
NicoRufini <nikorufini@gmail.com>
2024-06-13 17:36:34 +0200

    00

--- Lezione16/esercizi_classi_ed_ereditarietà.py
+++ Lezione16/esercizi_classi_ed_ereditarietà.py
@@ -367,19 +367,22 @@ class Specie:
         self.tasso_crescita = tasso_crescita
         
     def cresci(self) -> None:
-        popolazione_nuova = self.popolazione_iniziale*(1 + self.tasso_crescita/100)
+        #popolazione_nuova = self.popolazione_iniziale*(1 + self.tasso_crescita/100)
+        self.popolazione_iniziale *= (1 + self.tasso_crescita/100)
         #self.popolazione_iniziale = popolazione_nuova
-        return popolazione_nuova #self.popolazione_iniziale #
+        #return popolazione_nuova #self.popolazione_iniziale #
     
     def anni_per_superare(self, altra_specie: 'Specie') -> int:
         anni: int = 0
         
         while self.popolazione_iniziale <= altra_specie.popolazione_iniziale and anni < 1000:
-            self.popolazione_iniziale = self.cresci()
+            #self.popolazione_iniziale = self.cresci()
             #altra_specie.popolazione_iniziale = self.cresci()
+            self.cresci()
+            altra_specie.cresci()
            
         
-            anni += 2
+            anni += 1 #2
             
         return anni
     
@@ -389,12 +392,16 @@ class Specie:
         anni: int = 0
         
         while densita < 1:
-            self.popolazione_iniziale = self.cresci()
-            self.popolazione_iniziale = self.cresci()
-            self.popolazione_iniziale = self.cresci()
-            self.popolazione_iniziale = self.cresci()
-            self.popolazione_iniziale = self.cresci()
+            #self.popolazione_iniziale = self.cresci()
+            #self.popolazione_iniziale = self.cresci()
+            #self.popolazione_iniziale = self.cresci()
+            #self.popolazione_iniziale = self.cresci()
+            #self.popolazione_iniziale = self.cresci()
+
+            self.cresci()
+
             densita = self.popolazione_iniziale/area_kmq
+            
             anni += 1
         
         return anni
@@ -411,7 +418,7 @@ class Elefante(Specie):
 
 print("(1° check)", "-"*15)
 # Creazione delle istanze delle specie
-bufalo_klingon = BufaloKlingon(100, 15)  # Crea un'istanza di BufaloKlingon con popolazione 100 e tasso di crescita 15%
+bufalo_klingon = BufaloKlingon(100, 16) ###(100, 15)  # Crea un'istanza di BufaloKlingon con popolazione 100 e tasso di crescita 15%
 elefante = Elefante(10, 35)  # Crea un'istanza di Elefante con popolazione 10 e tasso di crescita 35%
 
 # Calcolo degli anni necessari per superare
@@ -419,5 +426,5 @@ anni_necessari = elefante.anni_per_superare(bufalo_klingon)  # Calcola gli anni 
 print(f"Anni necessari perché la popolazione di elefanti superi quella dei bufali Klingon: {anni_necessari}") #Expected: Anni necessari perché la popolazione di elefanti superi quella dei bufali Klingon: 16
 
 # Calcolo della densità di popolazione per i Bufali Klingon
-anni_densita = bufalo_klingon.getDensita(1500)  # Calcola gli anni necessari per raggiungere una densità di 1 bufalo Klingon per km²
+anni_densita = bufalo_klingon.getDensita(1700) ###(1500)  # Calcola gli anni necessari per raggiungere una densità di 1 bufalo Klingon per km²
 print(f"Anni necessari per raggiungere una densità di 1 Bufalo Klingon per km quadrato: {anni_densita}") #Expected: Anni necessari per raggiungere una densità di 1 Bufalo Klingon per km quadrato: 4
