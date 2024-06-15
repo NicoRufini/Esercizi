c0d3bb53b339edd67ddc7cbe44b602968559219d
NicoRufini <nikorufini@gmail.com>
2024-06-15 22:10:08 +0200

    b

---
+++ Lezione17/film.py
@@ -0,0 +1,28 @@
+'''
+Blockbuster
+
+### CLASSE: Film
+In un file chiamato "film.py", si definisca la classe Film che rappresenta un film preso a nolleggio. In tale classe, definire un codice identificativo (int) ed un titolo (string). Entrambi gli attributi sono da considerarsi privati.
+ 
+- Definire i seguenti metodi:
+
+    init(id, title): metodo costruttore.
+    setID(id): che consente di impostare il codice identificativo del film, modificando il valore del relativo attributo.
+    setTitle(title): che consente di impostare il codice identificativo del film, modificando il valore del relativo attributo.
+    getID(): che consente di ritornare il valore del codice identificativo di un film.
+    getTitle(): che consente di ritornare il valore del titolo di un film.
+    isEqual(otherFilm): che ritorna true se il codice identificativo di due film è uguale.  
+
+ 
+### CLASSI GENERE
+Si creino tre classi chiamate Azione, Commedia e Drama, tutte derivanti dalla classe Film. Ognuna di queste classi ha un attributo privato di tipo string chiamato genere, che equivale al genere di film (genere="Azione", nella classe Azione) ed un attributo privato di tipo float chiamato penale. I film di azione hanno una penalità di 3 euro al giorno, le commedie hanno una penale di 2.50 euro al giorno, i film drammatici hanno una penale di 2 euro al giorno.
+
+- Per ognuna di queste classi si implementi un metodo chiamato:
+
+    getGenere(), che ritorna il genere di film
+    getPenale(), che ritorna il valore della penale
+    calcolaPenaleRitardo(), che prende in ingresso il numero dei giorni di ritardo per un film e restituisce la penale da pagare per quel film.
+
+Le tre classi Azione, Commedia e Drama devono essere contenute nel file "movie_genre.py".
+
+'''
